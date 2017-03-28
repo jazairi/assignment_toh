bd90a7448d86945f4517c2b4259c96ce5d6a296f
Adam Jazairi <adamjazairi@gmail.com>
2017-03-27 21:10:01 -0400

    List other methods

--- towers.rb
+++ towers.rb
@@ -9,6 +9,36 @@ class TowerOfHanoi
     @board[0] = (1..towers).to_a
   end
 
+  def play
+    puts "Welcome to Tower of Hanoi!\n
+    Instructions:\n
+    Enter where you'd like to move from and to\n
+    in the format '1,3'. Enter 'q' to quit."
 
+    set_up
+    move
+  end
+
+  def render
+    # Display current board state
+  end
+
+  def get_move
+    render
+
+    # Prompt user for move
+  end
+
+  def execute_move
+    # Execute move
+  end
+
+  def valid?
+    # Validate move
+  end
+
+  def won?
+    # Victory conditions
+  end
 
 end
