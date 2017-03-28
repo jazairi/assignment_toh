c9eb3da9d0ab56edc68f65f1034232e4a7f7bcfd
Adam Jazairi <adamjazairi@gmail.com>
2017-03-27 21:52:38 -0400

    Prompt for user input

--- towers.rb
+++ towers.rb
@@ -5,19 +5,22 @@ class TowerOfHanoi
   end
 
   def set_up
+    # Set up board as 2D array
     @board = Array.new(towers, [])
     @board[0] = (1..towers).to_a
   end
 
   def play
+    # Display instructions
     puts "Welcome to Tower of Hanoi!\n
     Instructions:\n
     Enter in numeric form where you'd like to move.\n
     Left position is 1, middle position is 2, right position is 3.\n
     Enter 'q' to quit."
 
+    # Set up board, then prompt for move
     set_up
-    move
+    get_move
   end
 
   def render
@@ -27,14 +30,30 @@ class TowerOfHanoi
   def get_move
     render
 
-    # Prompt user for move
+    puts "Move from: "
+    start_position = gets.chomp
+    exit if start_position.downcase == 'q'
+    start_position.to_i
+
+    puts "Move to: "
+    end_position = gets.chomp
+    exit if end_position.downcase == 'q'
+    end_position.to_i
+
+    # Play move if valid, otherwise get new move
+    if valid?(start_position, end_position)
+      execute_move
+    else
+      puts "Invalid move!"
+      retry
+    end
   end
 
-  def execute_move(start, end)
+  def execute_move(start_position, end_position)
     # Execute move
   end
 
-  def valid?
+  def valid?(start_position, end_position)
     # Validate move
   end
 
